eafc6213e7a782ecd941c889b1d28c66b97694cb
Yumi <74994409+YumiKimbara@users.noreply.github.com>
2026-06-05 18:32:24 -0700

    create an Identicon module to convert string into struct named hex

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,13 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for `Identicon`.
-  """
+  def main(input) do
+    input
+    |> hash_input # pass input to hash_input using pipe operator
+  end
 
-  @doc """
-  Hello world.
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input) # use Erlang module to convert string to md5 hash
+    |> :binary.bin_to_list # convert binary to list
 
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    %Identicon.Image{hex: hex} # creates a struct (a special map) of type Identicon.Image
   end
 end
