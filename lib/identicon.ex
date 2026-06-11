6414642d5ec57f922498ab35837912809669f9ce
Yumi <74994409+YumiKimbara@users.noreply.github.com>
2026-06-11 14:26:02 -0700

    create pick_color module

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,7 +2,23 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input # pass input to hash_input using pipe operator
+    |> pick_color
   end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do # | _tail means I acknowledge there are remaining values, but I ignore them.
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  # if pick_color module is a JS function
+  # pick_color: function(image) {
+  #   image.color = {
+  #     r: image.hex[0],
+  #     g: image.hex[1],
+  #     b: image.hex[2]
+  #   }
+
+  #   return image
+  # }
 
   def hash_input(input) do
     hex = :crypto.hash(:md5, input) # use Erlang module to convert string to md5 hash
